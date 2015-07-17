aba668c4f4431b15d1f4e861768c7c0d7bcc97c7
atechapart <a.sanchez@atechapart.com>
2015-07-17 04:13:12 +0000

    clean up and refactor custom.js code responsive-slide-out

--- custom.js
+++ custom.js
@@ -4,7 +4,7 @@ origin: ransom carroll, for the goodvibes theme by cart designers
 ver: 1.x
 */
 
-// start trigger responsive slide out navigation
+// ready
 $(document).ready(function(){
     responsiveSlideOut.init();
 });
@@ -13,65 +13,43 @@ $(document).ready(function(){
 var responsiveSlideOut = {
     init: function(){
         if(window.innerWidth < 640){
-            responsiveSlideOut.buildPagesMenu();
+            responsiveSlideOut.triggerEvents();
+            responsiveSlideOut.renderResponsiveSlideOutInterface();
             responsiveSlideOut.togglefreezePageScroll();
             responsiveSlideOut.toggleSearchField();
         }
     },
     configs: {
-        /*this fixes the issue where on mobile when you scroll down, some elements jump out of design because the address bar disappears, this freezes the entire document's html when the slide out navigation is active to lock elements into place including the chrome address bar*/
+        //configure overflow: hidden; to toggle dynamically on <html> element: 
+        //bug fix for {https://teamtreehouse.com/forum/position-fixed-css-bug-in-chrome-and-firefox-for-android}
         freezePageScrollScroll: false
     }, 
-    buildPagesMenu: function(){
-        /*start create the entire left pane that will contain all the other stuff*/
-        var leftPaneHTML = '<div class="left-pane external-pane"></div>';
-        $('html').append(leftPaneHTML);
-        /*end create the entire left pane that will contain all the other stuff*/
-
-        /*start create call to action item*/
-        /*the dir="ltr" here is used because for whatever reasons, the phone number mixed with text displays in reverse direction*/
-        var callToActionItem = '<div class="call-to-action-item"><ul><li><a href="tel:+1-866-227-2537" dir="ltr"><i class="fa fa-phone"></i>866-2-BRAKES</a><small>(866-227-2537)</small></li><li>Se Habla Español</li></ul></div>';
-        /*refactor this selector into a variable*/
-        $('.external-pane').prepend(callToActionItem);
-        /*end create call to action item*/
-
-        /*start create pages menu*/
-        var pageMenuContainer = '<div class="page-menu-container"></div>';
-        // $('.external-pane .page-menu-container').append($('ul.pages-menu').clone());
-        $('.external-pane').append(pageMenuContainer);
-        $('#DrawerMenu ul.pages-menu').clone().appendTo($(".page-menu-container"));
-
-        /*end create pages menu*/
-
-        // var pagesMenuItems = '<div class="pages-menu pages-pane pane-menu"></div>';
-
-        // var categoriesPane = '<div class="categories-pane pane-menu"><h3 class="border-color-primary">Browse By Category</h3></div>';
-
-        // var categoryMenu = $('.CategoryList > div > ul').clone().appendTo($('.categories-pane'));
-
-        /* Cleanup */
-        $('.pane-menu div').remove();
-
-        $('.pane-menu ul').each(function(){
-            var self = $(this);
-            if(!self.children('li').length){
-                self.remove();
-            } else {
-                self.removeAttr('style');
-            }
-        });
-
-        // button toggle event
-        $('.button__toggle').on('click',function(){
-            responsiveSlideOut.toggleLeftPane();
-            $("#nav__responsive").toggleClass('nav__responsive--toggle');
-            $(".search__bar").toggleClass('nav__responsive--toggle');
+    triggerEvents: function() {
+        //when the hamburger button is clicked
+        $('.slideout__button--hamburger').on('click',function(){
+            //slide out the panel
+            responsiveSlideOut.toggleLeftSlideOutPanel();
+            //and freeze the page to prevent scrolling while slide out panel is active
             responsiveSlideOut.togglefreezePageScroll();
 
-
+            //refactor this to be more abstract
+            $("#nav__responsive").toggleClass('nav__responsive--toggle');
+            $(".search__bar").toggleClass('nav__responsive--toggle');
         });
+    }, 
+    renderResponsiveSlideOutInterface: function(){
+        /*render slideout panel*/        
+        var renderSlideOutPanelHtml = '<div class="left-slide-out-panel slide-out-panel"></div>';
+        $('html').append(renderSlideOutPanelHtml);
+
+        /*render head content block in slideout panel*/
+        /*the dir="ltr" used in the anchor tag is used to fix bug where the phone number text with hyphens renders out of order*/
+        var renderHeadContentBlockHtml = '<div class="call-to-action-item"><ul><li><a href="tel:+1-866-227-2537" dir="ltr"><i class="fa fa-phone"></i>866-2-BRAKES</a><small>(866-227-2537)</small></li><li>Se Habla Español</li></ul></div>';
+        /*refactor this selector into a variable*/
+        $('.slide-out-panel').prepend(renderHeadContentBlockHtml);
+        /*end create call to action item*/
     },
-    toggleLeftPane: function(){
+    toggleLeftSlideOutPanel: function(){
 
         var html = $('html');
         var searchBar = $('.search__bar');
@@ -79,25 +57,25 @@ var responsiveSlideOut = {
         if(html.hasClass('rightopen')){
             // responsive.toggleRightPane();
             var delayedOpenLeft = window.setTimeout(function(){
-                html.toggleClass('leftopen');
+                html.toggleClass('left-slide-out-panel-open');
             }, 100);
         } else {
-            html.toggleClass('leftopen');
+            html.toggleClass('left-slide-out-panel-open');
             //             $("html").css({"overflow":"hidden"});
-            // searchBar.toggleClass('leftopen');
+            // searchBar.toggleClass('left-slide-out-panel-open');
             // searchBar.css({"left":"295px"});
         }
     },
     checkLeftPaneForOpen: function(){
-        if($('html').hasClass('leftopen')){
+        if($('html').hasClass('left-slide-out-panel-open')){
             return true;
         } else {
             return false;
         }
     },
     togglefreezePageScroll: function(){
-         
-        $('.button__toggle').on('click',function(){            
+
+        $('.slideout__button--hamburger').on('click',function(){            
             if (responsiveSlideOut.configs.freezePageScrollScroll === false) {  
                 responsiveSlideOut.freezePageScroll(); 
             } else {                 
